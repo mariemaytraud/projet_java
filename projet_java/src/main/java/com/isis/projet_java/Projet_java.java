ab3b5c7504e3e92c2849afed01ba58a23e9645ca
mariemaytraud <maytraudmarie@gmail.com>
2025-03-12 15:22:05 +0100

    Premier commit

---
+++ projet_java/src/main/java/com/isis/projet_java/Projet_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.isis.projet_java;
+
+/**
+ *
+ * @author MARIE
+ */
+public class Projet_java {
+
+    public static void main(String[] args) {
+        System.out.println("a!");
+    }
+}
