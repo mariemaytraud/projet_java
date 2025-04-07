fa5d8f59acd596be4018b0fd358591332d4ffbcd
mariemaytraud <maytraudmarie@gmail.com>
2025-04-07 16:53:15 +0200

    c'est tout bon

--- projet_java/src/main/java/pendu/ClavierTestPanel.java
+++ projet_java/src/main/java/pendu/ClavierTestPanel.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 /**
  *
  * @author MARIE
@@ -16,23 +17,25 @@ import java.awt.event.ActionListener;
 public class ClavierTestPanel extends JPanel {
 
     private Test jeu ;  // Référence au jeu principal
+    ArrayList<JButton> boutons;
 
     public ClavierTestPanel(Test jeu) {
         this.jeu = jeu;  // Permet de communiquer avec la fenêtre principale
-
+        this.boutons = new ArrayList<>();
         setLayout(new GridLayout(3, 9, 5, 5));  // 3 lignes, 9 colonnes, avec un espacement
 
         // Créer les boutons pour chaque lettre de A à Z
         for (char lettre = 'A'; lettre <= 'Z'; lettre++) {
             JButton btnLettre = new JButton(String.valueOf(lettre));
+            boutons.add(btnLettre);
             btnLettre.setFont(new Font("Arial", Font.BOLD, 16));
 
             // pour détecter les clics
             btnLettre.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    jeu.verifierLettre(btnLettre.getText().charAt(0));  // Appel de la méthode
                     btnLettre.setEnabled(false);  // Désactive le bouton après utilisation
+                    jeu.verifierLettre(btnLettre.getText().charAt(0));  // Appel de la méthode
                 }
             });
 
@@ -41,11 +44,8 @@ public class ClavierTestPanel extends JPanel {
     }
     
 public void resetClavier() {
-    for (Component comp : getComponents()) {
-        if (comp instanceof JButton) {
-            JButton bouton = (JButton) comp;
+    for (var bouton : boutons) {
             bouton.setEnabled(true);
         }
     }
-}
 }
