741945416eafd7d505c772d0cbedaa59b2fea4da
salva <lamouscouli.labostrofa@gmail.com>
2025-04-09 23:04:05 +0200

    code de dessin lié aux boutons mais marche pas forcement v2

--- projet_java/src/main/java/dessin/DessinPanel.java
+++ projet_java/src/main/java/dessin/DessinPanel.java
@@ -6,21 +6,27 @@ package dessin;
 
 import acceuil.EntetePanel;
 import acceuil.MainFrame;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Alexis Burgos
  */
 public class DessinPanel extends JPanel {
-
-    private ArdoisePanel ardoise;
+    
     private Color couleur;
-    private int niveau = 1; // Le niveau sera défini par l'extérieur
-
+    private int niveau = 1;
+    private ArdoisePanel ardoise;
+    private MainFrame parentFrame;
+    private EntetePanel entete;
+    
+    // Boutons de dessin
     private JButton vert;
     private JButton rouge;
     private JButton bleu;
@@ -32,47 +38,72 @@ public class DessinPanel extends JPanel {
     private JButton taille15;
     private JButton gomme;
     private JButton effacer;
+    private JPanel fonctionsPanel;
 
     public DessinPanel(MainFrame parentFrame) {
-        this.setLayout(new BorderLayout(5, 5));
-        ardoise = new ArdoisePanel(); // Initialisation de l'ardoise
-        this.add(ardoise, BorderLayout.CENTER);
-
-        creerComposants(); // Créer les composants de contrôle (boutons)
-        afficherSelonNiveau(); // Afficher les composants en fonction du niveau initial
-        this.setBackground(new Color(122, 89, 201));
-
-        // Ajouter l'entête
-        EntetePanel entetePanel = new EntetePanel("Ardoise Magique", parentFrame);
-        this.add(entetePanel, BorderLayout.NORTH);
+        this.parentFrame = parentFrame;
+        initGui();
     }
 
+    private void initGui() { 
+        // Configuration du layout principal
+        BorderLayout bl = new BorderLayout(5, 5);
+        this.setLayout(bl);
+        
+        // Création de l'ardoise (zone de dessin)
+        ardoise = new ArdoisePanel();
+        this.add(ardoise, BorderLayout.CENTER);
+        
+        // Création du panneau de fonctions (boutons)
+        fonctionsPanel = createFonctionsPanel();
+        this.add(fonctionsPanel, BorderLayout.EAST);
+        
+        // Création de l'entête avec un titre approprié
+        entete = new EntetePanel("Dessin", parentFrame);
+        this.add(entete, BorderLayout.NORTH);
+        
+        // Couleur de fond
+        this.setBackground(new Color(122, 89, 201));
+    }
+    
+    private JPanel createFonctionsPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(0, 1));
+        
+        // Création des composants
+        creerComposants();
+        
+        // Afficher les composants selon le niveau
+        afficherSelonNiveau(panel);
+        
+        return panel;
+    }
+    
     private void creerComposants() {
-        // Initialisation des boutons de couleurs
         vert = new JButton("Vert");
         vert.setBackground(Color.GREEN);
-        vert.addActionListener(e -> {
-            ardoise.setCouleurStylo(Color.green);
+        vert.addActionListener((e) -> {
+            ardoise.setCouleurStylo(Color.GREEN);
             ardoise.setTailleStylo(2);
         });
 
         rouge = new JButton("Rouge");
         rouge.setBackground(Color.RED);
-        rouge.addActionListener(e -> {
+        rouge.addActionListener((e) -> {
             ardoise.setCouleurStylo(Color.RED);
             ardoise.setTailleStylo(2);
         });
 
         bleu = new JButton("Bleu");
         bleu.setBackground(Color.BLUE);
-        bleu.addActionListener(e -> {
-            ardoise.setCouleurStylo(Color.blue);
+        bleu.addActionListener((e) -> {
+            ardoise.setCouleurStylo(Color.BLUE);
             ardoise.setTailleStylo(2);
         });
 
         palette = new JButton("Palette de couleurs");
         palette.setBackground(Color.CYAN);
-        palette.addActionListener(e -> {
+        palette.addActionListener((e) -> {
             Color couleur = JColorChooser.showDialog(null, "Choisir une couleur", ardoise.getcouleurstylo());
             if (couleur != null) {
                 ardoise.setCouleurStylo(couleur);
@@ -82,67 +113,95 @@ public class DessinPanel extends JPanel {
 
         gomme = new JButton("Gomme");
         gomme.setBackground(Color.LIGHT_GRAY);
-        gomme.addActionListener(e -> {
-            ardoise.setCouleurStylo(Color.white);
+        gomme.addActionListener((e) -> {
+            ardoise.setCouleurStylo(Color.WHITE);
             ardoise.setTailleStylo(20);
         });
 
         effacer = new JButton("Tout effacer");
         effacer.setBackground(Color.LIGHT_GRAY);
-        effacer.addActionListener(e -> ardoise.effacer());
+        effacer.addActionListener((e) -> {
+            ardoise.effacer();
+        });
 
         tailleLabel = new JLabel("Taille du stylo:");
         tailleLabel.setForeground(Color.BLACK);
 
         taille2 = new JButton("2");
         taille2.setBackground(Color.LIGHT_GRAY);
-        taille2.addActionListener(e -> ardoise.setTailleStylo(2));
+        taille2.addActionListener((e) -> {
+            ardoise.setTailleStylo(2);
+        });
 
         taille5 = new JButton("5");
         taille5.setBackground(Color.LIGHT_GRAY);
-        taille5.addActionListener(e -> ardoise.setTailleStylo(5));
+        taille5.addActionListener((e) -> {
+            ardoise.setTailleStylo(5);
+        });
 
         taille10 = new JButton("10");
         taille10.setBackground(Color.LIGHT_GRAY);
-        taille10.addActionListener(e -> ardoise.setTailleStylo(10));
+        taille10.addActionListener((e) -> {
+            ardoise.setTailleStylo(10);
+        });
 
         taille15 = new JButton("15");
         taille15.setBackground(Color.LIGHT_GRAY);
-        taille15.addActionListener(e -> ardoise.setTailleStylo(15));
+        taille15.addActionListener((e) -> {
+            ardoise.setTailleStylo(15);
+        });
     }
-
+    
     // Méthode pour afficher les composants en fonction du niveau
-    public void afficherSelonNiveau() {
-        this.removeAll(); // Supprimer tous les composants précédents
+    private void afficherSelonNiveau(JPanel panel) {
+        panel.removeAll(); // Supprimer tous les composants
 
         if (niveau == 1) {
-            // Niveau 1 : Configuration pour un simple dessin
-            ardoise.setCouleurStylo(Color.BLUE); // Définir la couleur initiale
-            this.add(vert);
-            this.add(bleu);
-            this.add(rouge);
-            this.add(gomme); // Ces boutons sont toujours présents
-            this.add(effacer);
+            // Mettre à jour le titre de l'entête pour refléter le niveau
+            entete.setTitre("Dessin - Niveau 1");
+            
+            ardoise.setCouleurStylo(Color.BLUE);
+            panel.add(vert);
+            panel.add(bleu);
+            panel.add(rouge);
+            panel.add(gomme);
+            panel.add(effacer);
         } else if (niveau == 2) {
-            // Niveau 2 : Configuration avec palette et tailles supplémentaires
-            ardoise.setCouleurStylo(Color.BLUE); // Définir la couleur initiale
-            this.add(palette);
-            this.add(gomme); // Ces boutons sont toujours présents
-            this.add(effacer);
-            this.add(tailleLabel);
-            this.add(taille2);
-            this.add(taille5);
-            this.add(taille10);
-            this.add(taille15);
+            // Mettre à jour le titre de l'entête pour refléter le niveau
+            entete.setTitre("Dessin - Niveau 2");
+            
+            ardoise.setCouleurStylo(Color.BLUE);
+            panel.add(palette);
+            panel.add(gomme);
+            panel.add(effacer);
+            panel.add(tailleLabel);
+            panel.add(taille2);
+            panel.add(taille5);
+            panel.add(taille10);
+            panel.add(taille15);
         }
 
-        revalidate(); // Recalculer la disposition des composants
-        repaint();    // Redessiner le panneau
+        panel.revalidate();
+        panel.repaint();
     }
-
-    // Méthode pour changer le niveau
+    
+    // Méthode pour mettre à jour le niveau et rafraîchir l'interface
     public void setNiveau(int nouveauNiveau) {
         this.niveau = nouveauNiveau;
-        afficherSelonNiveau(); // Mettre à jour les composants en fonction du nouveau niveau
+        System.out.println("DessinPanel - Nouveau niveau : " + this.niveau);
+        afficherSelonNiveau(fonctionsPanel);
+    }
+    
+    public int getNiveau() {
+        return niveau;
+    }
+    
+    public Color getColor() {
+        return couleur;
+    }
+    
+    // Méthode pour accéder à l'ardoise
+    public ArdoisePanel getArdoise() {
+        return ardoise;
     }
 }
